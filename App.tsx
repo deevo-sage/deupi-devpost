5701579b35f8d3f4690f1e007297c95cf099c018
sidharth sahni <54073655+deevo-sage@users.noreply.github.com>
2022-04-14 20:44:54 +0530

    added qr scanner

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
+import {} from 'expo';
 import useCachedResources from './hooks/useCachedResources';
 import Navigation from './navigation';
-
 export default function App() {
   const isLoadingComplete = useCachedResources();
-
+  useEffect(() => {}, []);
   if (!isLoadingComplete) {
     return null;
   } else {
--- screens/main/home.tsx
+++ screens/main/home.tsx
@@ -1,46 +1,58 @@
-import { Avatar, Divider, Flex, Pressable, Text, View } from "native-base";
-import React, { FC, useEffect } from "react";
-import axios from "axios";
+import {
+  Actionsheet,
+  Avatar,
+  Button,
+  Divider,
+  Flex,
+  Pressable,
+  Text,
+  View,
+} from 'native-base';
+import React, { FC, useEffect, useState } from 'react';
+import axios from 'axios';
 
-import { Feather } from "@expo/vector-icons";
-import { shrinkAddress } from "../../utils";
+import { Feather } from '@expo/vector-icons';
+import { shrinkAddress } from '../../utils';
+import { RNCamera } from 'react-native-camera';
+import { Camera } from 'expo-camera';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import { Linking } from 'react-native';
 interface HomeProps {}
 
 export const Home: FC<HomeProps> = ({}) => {
-  const address = "0x1dd8d38e294d632eab2d445beac8340462db021d";
-  const accountName = "Account 1";
-  const Balance = "$11.8";
-
+  const address = '0x1dd8d38e294d632eab2d445beac8340462db021d';
+  const accountName = 'Account 1';
+  const Balance = '$11.8';
   return (
     <Flex pt="4">
-      <Flex align={"center"}>
+      <Flex align={'center'}>
         <View
           borderWidth={2}
           borderColor="blue.500"
-          borderRadius={"full"}
+          borderRadius={'full'}
           p="0.5"
           mb="2"
         >
           <Avatar
-            size={"md"}
+            size={'md'}
             bgColor="white"
             source={{
               uri:
-                "https://avatars.dicebear.com/api/identicon/" +
+                'https://avatars.dicebear.com/api/identicon/' +
                 address +
-                ".svg",
+                '.svg',
             }}
           />
         </View>
-        <Text mb="2" fontSize={"xl"} fontWeight="semibold">
+        <Text mb="2" fontSize={'xl'} fontWeight="semibold">
           {accountName}
         </Text>
-        <Text mb="4" fontSize={"sm"}>
+        <Text mb="4" fontSize={'sm'}>
           {Balance}
         </Text>
         <Pressable>
           <View
-            borderRadius={"full"}
+            borderRadius={'full'}
             py="1"
             px="2"
             bgColor="rgba(56, 189, 248,0.1)"
@@ -51,31 +63,45 @@ export const Home: FC<HomeProps> = ({}) => {
         <UtilButtons />
       </Flex>
       <Divider mt="4" />
-      <Flex>
+      <Flex w="100%" h="500">
         <Transactions address={address} />
       </Flex>
     </Flex>
   );
 };
+const QrScanner = ({ isOpen, onClose }) => {
+  return (
+    <Actionsheet isOpen={isOpen} onClose={onClose}>
+      <Flex backgroundColor={'gray.900'} py="4" w="100%" align={'center'}>
+        <BarCodeScanner
+          onBarCodeScanned={(item) => {
+            Linking.openURL(item.data);
+          }}
+          style={{ width: 300, height: 300 }}
+        />
+      </Flex>
+    </Actionsheet>
+  );
+};
 const Transactions = ({ address }) => {
   var data = {
-    jsonrpc: "2.0",
+    jsonrpc: '2.0',
     id: 0,
-    method: "alchemy_getAssetTransfers",
+    method: 'alchemy_getAssetTransfers',
     params: [
       {
         fromAddress: address,
-        contract: "0x0000000000000000000000000000000000001010",
-        category: ["erc20", "token"],
+        contract: '0x0000000000000000000000000000000000001010',
+        category: ['erc20', 'token'],
       },
     ],
   };
 
   var config = {
-    method: "post",
-    url: "https://polygon-mumbai.g.alchemy.com/v2/shDMEU7o9LPri4A4dpwR7wDGAyTTOi1m",
+    method: 'post',
+    url: 'https://polygon-mumbai.g.alchemy.com/v2/shDMEU7o9LPri4A4dpwR7wDGAyTTOi1m',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     data: data,
   };
@@ -93,14 +119,19 @@ const Transactions = ({ address }) => {
     console.log(address);
     GetTransaction();
   }, []);
-  return <Flex></Flex>;
+
+  const [x, setX] = useState('peepee ');
+  return <Flex w="100%" h="100%"></Flex>;
 };
 const UtilButtons = () => {
+  const [sheet, setSheet] = useState(false);
+
   return (
-    <Flex mt="4" direction="row" justify={"space-between"}>
-      <UtilButton text="Scan" Icon="maximize" />
+    <Flex mt="4" direction="row" justify={'space-between'}>
+      <UtilButton text="Scan" Icon="maximize" onPress={() => setSheet(true)} />
       <UtilButton text="Recieve" Icon="arrow-down" />
       <UtilButton text="Send" Icon="arrow-up-right" />
+      <QrScanner isOpen={sheet} onClose={() => setSheet(false)} />
     </Flex>
   );
 };
@@ -116,20 +147,20 @@ const UtilButton: FC<UtilButtonProps> = ({
 }) => {
   return (
     <Pressable onPress={onPress}>
-      <Flex align={"center"}>
+      <Flex align={'center'}>
         <Flex
-          align={"center"}
+          align={'center'}
           justify="center"
-          bgColor={"blue.500"}
-          borderRadius={"full"}
+          bgColor={'blue.500'}
+          borderRadius={'full'}
           mx="4"
           h="8"
           w="8"
-          fontWeight={"bold"}
+          fontWeight={'bold'}
         >
-          <Feather color={"white"} size={18} name={Icon} />
+          <Feather color={'white'} size={18} name={Icon} />
         </Flex>
-        <Text color={"blue.500"} fontSize="xs" fontWeight="bold" mt="1">
+        <Text color={'blue.500'} fontSize="xs" fontWeight="bold" mt="1">
           {text}
         </Text>
       </Flex>
