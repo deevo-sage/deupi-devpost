24ed183d9b1b6e57beb8b4181cabc6a2a6219f21
sidharth sahni <54073655+deevo-sage@users.noreply.github.com>
2022-04-16 04:27:46 +0530

    added abi

--- utils/web3.ts
+++ utils/web3.ts
@@ -1,5 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { ethers, providers} from 'ethers';
+import { ethers, providers } from 'ethers';
 import { Chains } from './types';
 
 export const killSession = async (set: any) => {};
@@ -23,3 +23,58 @@ export const provider = {
 export const getProvider = (val: Chains = 'maticmum') => {
   return new providers.AlchemyProvider(val, provider[val]);
 };
+
+export const ABI = [
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'previousOwner',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'newOwner',
+        type: 'address',
+      },
+    ],
+    name: 'OwnershipTransferred',
+    type: 'event',
+  },
+  {
+    inputs: [],
+    name: 'owner',
+    outputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'renounceOwnership',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'newOwner',
+        type: 'address',
+      },
+    ],
+    name: 'transferOwnership',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+];
